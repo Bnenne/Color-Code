9cc9daf0dc73742f79b0f873980137f4534efdf7
Benjamin Garcia <benjg208@gmail.com>
2024-12-03 19:02:26 -0600

    added /compare/data

--- src/App.vue
+++ src/App.vue
@@ -35,12 +35,57 @@ const toast = useToast()
           label: data.general.label[key]
         }));
         massesData.value = Object.keys(data.masses.auto_score).map(key => ({
-          x: parseFloat(data.masses.x[key].toFixed(1)),
-          y: parseFloat(data.masses.y[key].toFixed(1)),
-          auto_score: parseFloat(data.masses.auto_score[key].toFixed(1)),
-          team: parseFloat(data.masses.team[key].toFixed(1)),
-          label: parseFloat(data.masses.label[key].toFixed(1))
+          x: data.masses.x[key].toFixed(2),
+          y: data.masses.y[key].toFixed(2),
+          auto_score: data.masses.auto_score[key].toFixed(2),
+          team: data.masses.team[key],
+          label: data.masses.label[key]
         }));
+      }
+      else if(dataType.value === '/compare/data'){
+        const data = await response.json()
+        generalData.value = []
+        massesData.value = []
+        combinedData.value = []
+        length.value = data.teams.length
+        data.teams.forEach(team => {
+          for (const category in team) {
+            const scores = team[category]?.auto_score
+            if (scores) {
+              if (category === 'general') {
+                generalData.value.push(
+                    Object.keys(scores).map(key => ({
+                      x: team[category]?.x?.[key],
+                      y: team[category]?.y?.[key],
+                      auto_score: scores[key],
+                      team: team[category]?.team?.[key],
+                      label: team[category]?.label?.[key]
+                    }))
+                )
+              }
+              if (category === 'masses') {
+                massesData.value.push(
+                    Object.keys(scores).map(key => ({
+                      x: team[category]?.x?.[key].toFixed(2),
+                      y: team[category]?.y?.[key].toFixed(2),
+                      auto_score: scores[key].toFixed(2),
+                      team: team[category]?.team?.[key],
+                      label: team[category]?.label?.[key]
+                    }))
+                )
+              }
+            }
+          }
+        })
+        console.log('data.combined', data.combined)
+        data.combined.forEach(point => {
+          point.x = point.x.toFixed(2)
+          point.y = point.y.toFixed(2)
+          point.auto_score = point.auto_score.toFixed(2)
+        })
+        combinedData.value = data.combined
+        compatibility.value = data.compatibility.toFixed(2)
+        console.log('compatibility.value', compatibility.value)
       }
       else {
         const data = await response.blob()
@@ -63,10 +108,18 @@ const toast = useToast()
   const routes2 = ref([
     {route: '/data', text: 'Data'},
     {route: '/graph', text: 'Graph'},
-    {route: '/compare/graph', text: 'Compare'}
+    {route: '/compare/data', text: 'Compare Data'},
+    {route: '/compare/graph', text: 'Compare Graph'}
   ])
-  const events = ref('')
-  const teams = ref('')
+  const events = ref([
+    {route: '/events', text: 'All'}
+  ])
+  const teams = ref([
+    {route: '/frc1710', text: '1710'},
+    {route: '/frc1730', text: '1730'},
+    {route: '/frc1986', text: '1986'},
+    {route: '/frc1987', text: '1987'}
+  ])
 
   const route = ref('/auto')
   const route2 = ref('')
@@ -76,9 +129,12 @@ const toast = useToast()
   const team3 = ref(null)
   const generalData = ref(null)
   const massesData = ref(null)
+  const combinedData = ref(null)
+  const compatibility = ref(0)
+  const length = ref(1)
 
   function routeChange() {
-    if (route2.value !== '/compare/graph') {
+    if (route2.value !== '/compare/graph' || route2.value !== '/compare/data') {
       team2.value = ''
       team3.value = null
       isError.value = ''
@@ -131,7 +187,7 @@ const toast = useToast()
     toast.add({ severity: 'info', summary: 'Info', detail: 'Verify that the team attended the event, as not all teams have attended every event.', life: 5000 });
   }
 const showDev = () => {
-  toast.add({ severity: 'info', summary: 'Info', detail: "This will turn on dev mode, if you're not a dev this will break the everything.", life: 5000 });
+  toast.add({ severity: 'info', summary: 'Info', detail: "This will turn on dev mode, if you're not a dev this will break everything.", life: 5000 });
 }
 </script>
 
@@ -201,7 +257,7 @@ const showDev = () => {
             />
             <Select
                 @change="onChange"
-                v-if="route2 === '/compare/graph' && teams !== ''"
+                v-if="(route2 === '/compare/graph' || route2 === '/compare/data') && teams !== ''"
                 filter
                 v-model="team2"
                 :options="teams"
@@ -211,14 +267,14 @@ const showDev = () => {
                 placeholder="Select a Team"
             />
             <Select
-                v-if="route2 !== '/compare/graph' && teams !== ''"
+                v-if="(route2 !== '/compare/graph' && route2 !== '/compare/data') && teams !== ''"
                 disabled
                 class="w-44 mr-2"
                 placeholder="Select a Team"
             />
             <Select
                 @change="onChange"
-                v-if="route2 === '/compare/graph' && teams !== ''"
+                v-if="(route2 === '/compare/graph' || route2 === '/compare/data') && teams !== ''"
                 filter show-clear
                 v-model="team3"
                 :options="teams"
@@ -228,7 +284,7 @@ const showDev = () => {
                 placeholder="Select a Team"
             />
             <Select
-                v-if="route2 !== '/compare/graph' && teams !== ''"
+                v-if="(route2 !== '/compare/graph' && route2 !== '/compare/data') && teams !== ''"
                 disabled
                 class="w-44 mr-2"
                 placeholder="Select a Team"
@@ -284,7 +340,74 @@ const showDev = () => {
             </Card>
           </SplitterPanel>
         </Splitter>
-        <img v-if="dataType !== '/data'" :src="imageUrl" alt="Fetched Image" />
+        <Splitter v-if="dataType === '/compare/data'" class="w-fit h-fit">
+          <SplitterPanel>
+            <Card class="h-full">
+              <template #title>General</template>
+              <template #content>
+                <DataTable :value="generalData[0]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <DataTable :value="generalData[1]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <DataTable v-if="length === 3" :value="generalData[2]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+              </template>
+            </Card>
+          </SplitterPanel>
+          <SplitterPanel>
+            <Card class="h-full">
+              <template #title>Masses</template>
+              <template #content>
+                <DataTable :value="massesData[0]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="Average X"></Column>
+                  <Column field="y" header="Average Y"></Column>
+                  <Column field="auto_score" header="Average Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <DataTable :value="massesData[1]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <DataTable v-if="length === 3" :value="massesData[2]">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <DataTable :value="combinedData">
+                  <Column field="team" header="Team"></Column>
+                  <Column field="x" header="X"></Column>
+                  <Column field="y" header="Y"></Column>
+                  <Column field="auto_score" header="Auto Pieces Scored"></Column>
+                  <Column field="label" header="Label"></Column>
+                </DataTable>
+                <h4 class="mx-auto">Compatibility</h4>
+                <ProgressBar style="height: 40px; margin-top: 16px; margin-bottom: 16px" :value="compatibility"></ProgressBar>
+              </template>
+            </Card>
+          </SplitterPanel>
+        </Splitter>
+        <img v-if="dataType === '/compare/graph' || dataType === '/graph'" :src="imageUrl" alt="Fetched Image" />
       </div>
   </div>
 </template>
